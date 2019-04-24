c376877b0855c81e3462150c76a5aebf07eb865c
Christian Torres Martinez <michelle.torres@inegi.org.mx>
2019-04-24 10:19:18 -0500

    diseño a preview al momento de guardar, código js se separa en funciones

--- sare_seg/src/main/webapp/WEB-INF/resource/js/main.js
+++ sare_seg/src/main/webapp/WEB-INF/resource/js/main.js
@@ -940,144 +940,126 @@ else
 
 }
 
-const validaCp=()=>{
-    sendAJAX(urlServices['serviceValCP'].url, {
-        'codigo': $("#e14_A").val(),
-        'cve_ent': $("#e03").val(),
-        'proyecto':1}, 
-    urlServices['serviceValCP'].type, function (data) 
-    {
-        if (data[0].operation) 
-        {
-            if (data[0].datos.result === false) 
-            {
-                
-            }
-            else
-            {
-                var myform = $('#frmSARE');
-                var disabled = myform.find(':input:disabled').removeAttr('disabled');
-                var d = myform.serialize();
-                d += "&tramo_control=" + dataUserFromLoginLocalStorage.tramo_control;
-                d += "&coord_x=" + xycoorsx + "&coord_y=" + xycoorsy;
-                var u = dataUserFromLoginLocalStorage.nombre;
-                var htmlDiv = "<div id='vista'> </div>";
-                const sizeScreen = screen.width <= '768' ? '90%' : '80%' 
-                
-            Swal.fire({
-              title: '<h2 style="border-bottom: 1px solid lightgray; padding-bottom:10px;">VISTA PRELIMINAR</h2>', 
-                    width: sizeScreen, 
-                    html: htmlDiv, 
-                    confirmButtonText: 'Aceptarr', 
-                    customClass: 'swal-wide', 
-                    confirmButtonColor: '#0f0f0f', 
-                    allowEscapeKey: false, 
-                    allowOutsideClick: false, 
-                    showConfirmButton: true,
-                    showCancelButton: true,
-                    showCloseButton: true, 
-                    onOpen: showViewPreliminar(d) 
-            }).then((result) => {
-              if (result.value) {
-                sendAJAX(urlServices['serviceSaveUEAlter'].url, {'proyecto':1,'obj': JSON.stringify(ObjectRequest),'usuario':u}, urlServices['serviceSaveUEAlter'].type, function (data) 
-                        {
-                           if (data[0].operation) 
-                           {
-                            if (data[0].datos.mensaje.type === 'false') 
-                            {
-                                swal.fire({
-                                        title: "Error",
-                                        text: data[0].datos.mensaje.messages,
-                                        showConfirmButton: true,
-                                        confirmButtonText: 'Aceptar',
-                                        confirmButtonColor: '#4d4d4d',
-                                        type: "error"
-                                    }); 
-                                    return;
-                            }
-                            else
-                            {
-                               cleanForm()
-                               MDM6('hideMarkers', 'identify');
-                               swal.fire(
-                                {
-                                            title: 'Guardado',
-                                            text: 'El punto ha sido almacenado correctamente',
-                                            type: "success",
-                                            confirmButtonColor: "#DD6B55",
-                                            allowEscapeKey: true,
-                                            allowOutsideClick: true,
-                                            html: true,
-                                            animation: true
-                                                    //timer: 2500
-                                });
-                            }
-                           }
-                           else
-                           {
-                             swal.fire
-                             ({
-                                            title: 'Error',
-                                            text: 'Error de conexion!',
-                                            type: "error",
-                                            confirmButtonColor: "#DD6B55",
-                                            allowEscapeKey: true,
-                                            allowOutsideClick: true,
-                                            html: true,
-                                            animation: true
-                                                    //timer: 2500
-                                });  
-                           }
-                          
-                        }, function (){
-                            swal.fire({
-                                title: 'Guardando',
-                                text: 'Almacenando información, por favor espere un momento',
-                                type: "info",
-                                allowEscapeKey: true,
-                                allowOutsideClick: true,
-                                html: true,
-                                animation: true
-                                        //timer: 2500
-                            });
-                        });
-                    
-                    }
-                }); 
-              }
-            }
-    },function (){
+const validaCp = () => {
+  sendAJAX(urlServices['serviceValCP'].url, 
+  {
+    'codigo': $("#e14_A").val(),
+    'cve_ent': $("#e03").val(),
+    'proyecto':1
+  }, 
+  urlServices['serviceValCP'].type, 
+  data => {
+    if (data[0].operation) {
+      if (data[0].datos.result === false) {
+          
+      }
+      else {
+        modalViewPreliminar()
         
-    });
+      }
+    }
+  }, () => { })
 }
 
-const showViewPreliminar=(d)=>{
-    loadTemplate('vista', "resources/templates/preview.html?frm=" + Math.random(),
-            function () {
-                d = d.replace(/Seleccione/g, '');
-                var dpv = d.split("&");
-                $.each(dpv, function (i, e) {
-                    var idobj = e.split("=");
-                    var a = decodeURIComponent(idobj[1]);
-                    a = a.replace(/\+/g, ' ');
-                    ObjectRequest[idobj[0]] = a;
-                    $("#" + idobj[0] + "_pv").text(a);
-                });
+const modalViewPreliminar = () => {
+  const myform = $('#frmSARE')
+  let disabled = myform.find(':input:disabled').removeAttr('disabled')
+  let d = myform.serialize()
+  d += "&tramo_control=" + dataUserFromLoginLocalStorage.tramo_control
+  d += "&coord_x=" + xycoorsx + "&coord_y=" + xycoorsy
+  const htmlDiv = "<div id='vista'> </div>"
+  const sizeScreen = screen.width <= '768' ? '90%' : '80%' 
+        
+  Swal.fire({
+    title: '<h2 class="txt-preliminar">VISTA PRELIMINAR</h2>', 
+    width: sizeScreen, 
+    html: htmlDiv, 
+    confirmButtonText: 'Aceptar', 
+    customClass: 'swal-wide', 
+    confirmButtonColor: '#0f0f0f', 
+    allowEscapeKey: false, 
+    allowOutsideClick: false, 
+    showConfirmButton: true,
+    showCancelButton: true,
+    showCloseButton: true, 
+    onOpen: showViewPreliminar(d) 
+  }).then ( result => handleShowResult(result) )
+}
+
+const showViewPreliminar = d => {
+  loadTemplate('vista', "resources/templates/preview.html?frm=" + Math.random(),
+    () => {
+      d = d.replace(/Seleccione/g, '')
+      var dpv = d.split("&")
+      $.each(dpv, function (i, e) {
+        var idobj = e.split("=")
+        var a = decodeURIComponent(idobj[1])
+        a = a.replace(/\+/g, ' ')
+        ObjectRequest[idobj[0]] = a
+        $("#" + idobj[0] + "_pv").text(a)
+      })
                 
-                ObjectRequest['Cvegeo2016'] = cve_geo2016;
-                ObjectRequest['Cvegeo'] = cve_geo;
-                ObjectRequest['CE'] = dataUserFromLoginLocalStorage.ce;
-                ObjectRequest['id_deftramo'] = dataUserFromLoginLocalStorage.tramoControl;
-                ObjectRequest['tramo_control'] = dataUserFromLoginLocalStorage.tramoControl;
-                ObjectRequest['mod_cat'] = mod_cat;
-                ObjectRequest['punteo'] = punteo;
-                ObjectRequest['coordx'] = xycoorsx;
-                ObjectRequest['coordy'] = xycoorsy;
-                ObjectRequest['coordx'] = xycoorsx;
-                ObjectRequest['coordy'] = xycoorsy;
-                ObjectRequest['Cveft'] = cveft;
-                ObjectRequest['e10_cvevial'] = e10_cve_vial;
-            });
+      ObjectRequest['Cvegeo2016'] = cve_geo2016
+      ObjectRequest['Cvegeo'] = cve_geo
+      ObjectRequest['CE'] = dataUserFromLoginLocalStorage.ce
+      ObjectRequest['id_deftramo'] = dataUserFromLoginLocalStorage.tramoControl
+      ObjectRequest['tramo_control'] = dataUserFromLoginLocalStorage.tramoControl
+      ObjectRequest['mod_cat'] = mod_cat
+      ObjectRequest['punteo'] = punteo
+      ObjectRequest['coordx'] = xycoorsx
+      ObjectRequest['coordy'] = xycoorsy
+      ObjectRequest['coordx'] = xycoorsx
+      ObjectRequest['coordy'] = xycoorsy
+      ObjectRequest['Cveft'] = cveft
+      ObjectRequest['e10_cvevial'] = e10_cve_vial
+    }
+  )
+}
+
+const handleShowResult = result => {
+  const user = dataUserFromLoginLocalStorage.nombre
+  if (result.value) {
+    sendAJAX(urlServices['serviceSaveUEAlter'].url, 
+    {
+      'proyecto':1,
+      'obj': JSON.stringify(ObjectRequest),
+      'usuario':user
+    }, 
+    urlServices['serviceSaveUEAlter'].type, 
+    data => {
+      if (data[0].operation) {
+        if (data[0].datos.mensaje.type === 'false') {
+          handleShowSaveAlert('error', 'Error', data[0].datos.mensaje.messages, false)
+          return;
+        }
+        else {
+          cleanForm()
+          MDM6('hideMarkers', 'identify')
+          handleShowSaveAlert('success', 'Guardado', 'El punto ha sido almacenado correctamente', true)
+        }
+      }
+      
+      else {
+        handleShowSaveAlert('error', 'Error', 'Error de conexión', true)
+      }
+    }, () => handleShowSaveAlert('info', 'Guardando', 'Almacenando información, por favor espere un momento', true)
+    )
+        
+  } //close if result.value
+}
+
+const handleShowSaveAlert = (type, title, text, animation) => {
+  swal.fire({
+    type,
+    title,
+    text,
+    showConfirmButton: false,
+    showCloseButton: true,
+    allowEscapeKey: true,
+    allowOutsideClick: true,
+    // html: true,
+    animation
+  }) 
 }
 
 const identify = (coor) => HandleWhatDoYouWantToDo(coor)
