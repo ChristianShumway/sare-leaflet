aace182209cc9f1c3fd09889054eedb7c48fc742
FABIOLA.RUIZ <FABIOLA.RUIZ@inegi.gob.mx>
2019-08-13 11:09:59 -0500

    id_deftramo
    se egrego el campo de id_deftramo para la funcion del guardado del nuevo punto

--- sare_db/src/main/java/mx/org/inegi/sare/sare_db/dto/cat_frente_geometria.java
+++ sare_db/src/main/java/mx/org/inegi/sare/sare_db/dto/cat_frente_geometria.java
@@ -11,31 +11,16 @@ package mx.org.inegi.sare.sare_db.dto;
  */
 public class cat_frente_geometria {
     private String cveFrente;
-    private String geometria;
-    private String cve_ent;
-    private String cve_mun;
-    private String cve_loc;
-    private String cve_ageb;
-    private String cve_mza;
-    private String nom_ent;
-    private String nom_mun;
-    private String nom_loc;
+    private String geometria;  
     private String cveft;
-
+   private String id_deftramo;
     public cat_frente_geometria() {
     }
 
-    public cat_frente_geometria(String cveFrente, String geometria, String cve_ent, String cve_mun, String cve_loc, String cve_ageb, String cve_mza, String nom_ent, String nom_mun, String nom_loc,String cveft) {
+    public cat_frente_geometria(String cveFrente, String geometria, String cveft,String id_deftramo) {
         this.cveFrente = cveFrente;
         this.geometria = geometria;
-        this.cve_ent = cve_ent;
-        this.cve_mun = cve_mun;
-        this.cve_loc = cve_loc;
-        this.cve_ageb = cve_ageb;
-        this.cve_mza = cve_mza;
-        this.nom_ent = nom_ent;
-        this.nom_mun = nom_mun;
-        this.nom_loc = nom_loc;
+        this.id_deftramo = id_deftramo;     
         this.cveft=cveft;
     }
 
@@ -56,70 +41,6 @@ public class cat_frente_geometria {
         this.geometria = geometria;
     }
 
-    public String getCve_ent() {
-        return cve_ent;
-    }
-
-    public void setCve_ent(String cve_ent) {
-        this.cve_ent = cve_ent;
-    }
-
-    public String getCve_mun() {
-        return cve_mun;
-    }
-
-    public void setCve_mun(String cve_mun) {
-        this.cve_mun = cve_mun;
-    }
-
-    public String getCve_loc() {
-        return cve_loc;
-    }
-
-    public void setCve_loc(String cve_loc) {
-        this.cve_loc = cve_loc;
-    }
-
-    public String getCve_ageb() {
-        return cve_ageb;
-    }
-
-    public void setCve_ageb(String cve_ageb) {
-        this.cve_ageb = cve_ageb;
-    }
-
-    public String getCve_mza() {
-        return cve_mza;
-    }
-
-    public void setCve_mza(String cve_mza) {
-        this.cve_mza = cve_mza;
-    }
-
-    public String getNom_ent() {
-        return nom_ent;
-    }
-
-    public void setNom_ent(String nom_ent) {
-        this.nom_ent = nom_ent;
-    }
-
-    public String getNom_mun() {
-        return nom_mun;
-    }
-
-    public void setNom_mun(String nom_mun) {
-        this.nom_mun = nom_mun;
-    }
-
-    public String getNom_loc() {
-        return nom_loc;
-    }
-
-    public void setNom_loc(String nom_loc) {
-        this.nom_loc = nom_loc;
-    }
-
     public String getCveft() {
         return cveft;
     }
@@ -127,6 +48,16 @@ public class cat_frente_geometria {
     public void setCveft(String cveft) {
         this.cveft = cveft;
     }
+
+    public String getId_deftramo() {
+        return id_deftramo;
+    }
+
+    public void setId_deftramo(String id_deftramo) {
+        this.id_deftramo = id_deftramo;
+    }
+    
+    
     
     
     
