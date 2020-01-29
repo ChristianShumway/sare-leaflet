f2c1b66aaf0866b0cae6d192905fd72a9020817f
lidia.vazquez <lidia.vazquez@inegi.org.mx>
2020-01-29 12:19:55 -0600

    renombre campos x y y en oracle

--- sare_db/src/main/java/mx/org/inegi/sare/Enums/BusquedaEnum.java
+++ sare_db/src/main/java/mx/org/inegi/sare/Enums/BusquedaEnum.java
@@ -15,7 +15,7 @@ public enum BusquedaEnum {
             +   "ue.e10, ue.e11, TRIM(ue.e11a) as e11a lpad(to_char(ue.tipo_e14),2,'0') "
             +   "tipo_e14, ue.e14, lpad(to_char(ue.tipo_e10_a),2,'0') tipo_e10_a "
             +   "ue.e10_a,lpad(to_char(ue.tipo_e10_b),2,'0') tipo_e10_b, ue.e10_b, lpad(to_char(ue.tipo_e10_c),2,'0') "
-            +   "ue.tipo_e10_c, ue.e10_c, ue.x as coorx, to_char(ue.y) as coory "
+            +   "ue.tipo_e10_c, ue.e10_c, ue.x_val as coorx, to_char(ue.y_val) as coory "
             +   "ue.e16 as descrubic, pre.st_sare estatus_punteo, ue.e12, ue.e19, ue.tipo_e19, ue.e20 "
             +   "ue.e13, TRIM(ue.e13a) as e13_a,ue.e14a as e14_a, --to_char(origen) "
             +   "'' origen, ue.ce as cestatal "
