c5ac18186521c94e18089ec15c7718169d49bf0a
TORRES VAZQUEZ LIDIA GUADALUPE <LIDIA.VAZQUEZ@inegi.org.mx>
2021-04-12 12:04:16 -0500

    pruebas mouse wheel y chinchetas

--- sare_seg/src/main/webapp/WEB-INF/resource/js/withLeaflet.js
+++ sare_seg/src/main/webapp/WEB-INF/resource/js/withLeaflet.js
@@ -13,66 +13,106 @@
 //   accessToken: 'your.mapbox.access.token'
 // }).addTo(mymap);
 
-
+var marker;
 var wmsLayer = L.tileLayer.wms('http://ows.mundialis.de/services/service?', {
-  layers: 'TOPO-OSM-WMS',
-  sphericalMercator: true
+    layers: 'TOPO-OSM-WMS',
+    sphericalMercator: true
 })//.addTo(map);
 
 var wmsLayerM = L.tileLayer.wms('http://gaia.inegi.org.mx/NLB/tunnel/wms/wms61?', {
-  layers: 'Hipsografico,MGE',
-  transparent: false,
-  format: 'image/png',
-  //cql_filter:"ambito='U'",
-  id: 'xpain.test-cach',
-  useCache: true,
-  crossOrigin: false,
-  sphericalMercator: true
+    layers: 'Hipsografico,MGE',
+    transparent: false,
+    format: 'image/png',
+    //cql_filter:"ambito='U'",
+    id: 'xpain.test-cach',
+    useCache: true,
+    crossOrigin: false,
+    sphericalMercator: true
 })//.addTo(map);
 
 var wmsLayerSare = L.tileLayer.wms('https://gaia.inegi.org.mx/NLB_CE/balancer.do?map=/opt/map/SARE_UEEPA_2020.map', {
-  layers: 'c103,c102,c100,c101a,wdenue,c103r,c107,c107r,c108',
-  transparent: false,
-  format: 'image/png',
-  // //cql_filter:"ambito='U'",
-  id: 'xpain.test-cach',
-  useCache: true,
-  crossOrigin: false,
-  sphericalMercator: true,
-  EDO:'00',
+    layers: 'c103,c102,c100,c101a,wdenue,c103r,c107,c107r,c108',
+    transparent: false,
+    format: 'image/png',
+    // //cql_filter:"ambito='U'",
+    id: 'xpain.test-cach',
+    useCache: true,
+    crossOrigin: false,
+    sphericalMercator: true,
+    EDO: '00',
 });
 
+
+
 var crs = new L.Proj.CRS(
-  'EPSG:900913',
-  // '+proj=utm +zone=33 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs',
-  '+proj=merc +a=6378137 +b=6378137 +lat_ts=0 +lon_0=0 +x_0=0 +y_0=0 +k=1 +units=m +nadgrids=@null +wktext +no_defs',
-  {
-    // resolutions: [8192, 4096, 2048, 1024, 512, 256, 128],
-    resolutions: [8192, 4096, 2048, 1024, 512, 256, 128,64,32,16,8,4,2,1,0.5, 0],
-    // resolutions: [8192, 4096, 2048], // 3 example zoom level resolutions
-    origin: [0, 0]
-  }
+        'EPSG:900913',
+        // '+proj=utm +zone=33 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs',
+        '+proj=merc +a=6378137 +b=6378137 +lat_ts=0 +lon_0=0 +x_0=0 +y_0=0 +k=1 +units=m +nadgrids=@null +wktext +no_defs',
+        {
+            // resolutions: [8192, 4096, 2048, 1024, 512, 256, 128],
+            resolutions: [8192, 4096, 2048, 1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1, 0.5, 0],
+            // resolutions: [8192, 4096, 2048], // 3 example zoom level resolutions
+            origin: [0, 0]
+        }
 );
 
 console.log(crs);
 
 var map = L.map('mapid', {
-  center: [21.541, -102.034],//[-17, -67],
-  zoom: 1,
-  maxZoom: 18,
-  layers: [wmsLayerM,wmsLayer,wmsLayerSare],
-  crs: crs,
-  continuousWorld: true,
-  worldCopyJump: false
+    center: [21.541, -102.034], //[-17, -67],
+    zoom: 1,
+    maxZoom: 18,
+    layers: [wmsLayerM, wmsLayer, wmsLayerSare],
+    crs: crs,
+    continuousWorld: false,
+    worldCopyJump: false,
+    //scrollWheelZoom: false
 });
 
+map.on('click', function (e) {
+
+    if (marker !== undefined) {
+        map.removeLayer(marker)
+    }
+    marker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
+
+    // alert(e.latlng);
+});
+/*window.addEventListener('keydown', function (event)
+ {
+ if (event.ctrlKey == true)
+ {
+ map.scrollWheelZoom.enable();
+ event.preventDefault();
+ if (event.originalEvent.detail > 0) {
+ console.log('Down');
+ } else {
+ console.log('Up');
+ }
+ }
+ });*/
+
+/*window.addEventListener('mousewheel', function (e) {
+    if (event.ctrlKey == true)
+    {
+        map.scrollWheelZoom.enable();
+        event.preventDefault();
+    }
+    if (map.scrollWheelZoom.enabled()) {
+        //map.scrollWheelZoom.disable();
+    } else {
+        alert("presion ctrl para hacer zoom")
+        //map.scrollWheelZoom.enable();
+    }
+},
+        false);*/
 console.log("map crs: " + map.options.crs.code);
 
 
 var baseMaps = {
-  "MGE": wmsLayerM,
-  "TOPO-OSM-WMS": wmsLayer,
-  "wdenue": wmsLayerSare,
+    "MGE": wmsLayerM,
+    "TOPO-OSM-WMS": wmsLayer,
+    "wdenue": wmsLayerSare,
 };
 
 
